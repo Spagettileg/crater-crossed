629a9e7200b8adc7ce28367ef3b48fe5c101b782
Ubuntu <ubuntu@ip-172-31-41-0.eu-west-1.compute.internal>
2021-02-05 17:50:12 +0000

    handleClick method defined in the Board component. Slice method is called to create a copy of the Squares array.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -23,6 +23,12 @@ class Board extends React.Component {
     };
   }
   
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+  
   renderSquare(i) {
     return (<Square value={this.state.squares[i]}
     onClick={() => this.handleClick(i)}
