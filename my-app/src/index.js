69cc5d078df23a70b7f9bd0d60a494829eb8c6b2
Ubuntu <ubuntu@ip-172-31-41-0.eu-west-1.compute.internal>
2021-02-05 16:19:28 +0000

    Arrow function added to Square component for improved coding convention.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('click')}>
         {this.props.value}
       </button>
     );
