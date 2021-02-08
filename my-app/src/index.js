6fbd65733bf2241ae7a86140e5a0100c562bad06
Ubuntu <ubuntu@ip-172-31-41-0.eu-west-1.compute.internal>
2021-02-08 15:20:24 +0000

    New constant variable moves added to Game render statement. Moves will allow the user to view the game history and start the game from a selected moment in history.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -79,6 +79,18 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+    
+    const moves = history.map((step, move) => {
+      const desc = move ?
+      'Go to move #' + move :
+      'Go to game start';
+    return (
+      <li>
+      <button onClick={() => this.jumpTo(move)}>{desc}</button>
+      </li>
+      );
+    });
+    
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -96,7 +108,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
