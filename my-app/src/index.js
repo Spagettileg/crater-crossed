4c141fd44d2c42a4c510d5b42c89e7985cae28c7
Ubuntu <ubuntu@ip-172-31-41-0.eu-west-1.compute.internal>
2021-02-08 10:09:03 +0000

    handleClick method updated to include xIsNext boolean command. User will see alternate X & O when clicking on a null square.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -18,14 +18,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
-      sIsNext: true,
+      xIsNext: true,
     };
   }
   
   handleClick(i) {
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
   
   renderSquare(i) {
