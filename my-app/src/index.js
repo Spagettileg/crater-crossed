dbf19a26da633c50f8df127a401e8f2cc02ae476
Ubuntu <ubuntu@ip-172-31-41-0.eu-west-1.compute.internal>
2021-02-09 21:43:12 +0000

    User comments updated to improve UI.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -193,7 +193,7 @@ function calculateWinner(squares) {
   return null;
 }
 
-// If you want to start measuring performance in your app, pass a function
+// To start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
